9fadb86e0697f036642e9a93eaa99b95e2709013
cathands <cathandsservice@gmail.com>
2024-01-31 00:33:39 +0900

    app update

--- app.py
+++ app.py
@@ -1,9 +1,65 @@
-from flask import Flask, render_template
+import os
+import re
+import unicodedata
+from flask import Flask, render_template, request, send_file
+import pandas as pd
 app = Flask(__name__, template_folder="./templates", static_folder="./static")
 
-@app.route('/upload')
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def normalize_data(file):
+    # str -> bytes
+    bytes_text = file.encode()
+    # 濁点Unicode結合文字置換
+    bytes_text = re.sub(b"\xe3\x82\x9b", b'\xe3\x82\x99', bytes_text)
+    bytes_text = re.sub(b"\xef\xbe\x9e", b'\xe3\x82\x99', bytes_text)
+    # 半濁点Unicode結合文字置換
+    bytes_text = re.sub(b"\xe3\x82\x9c", b'\xe3\x82\x9a', bytes_text)
+    bytes_text = re.sub(b"\xef\xbe\x9f", b'\xe3\x82\x9a', bytes_text)
+    # byte_text -> str
+    file = bytes_text.decode()
+    # 正規化
+    file = unicodedata.normalize("NFC", file)
+    return file
+
+@app.route('/')
 def Home():
     return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def handle_upload():
+    try:
+        file = request.files['file']
+        if file:
+            # アップロードされたファイルを保存
+            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filename)
+
+            # 正規化されたファイル名を生成
+            re_prefixed_filename = f"re.{file.filename}"
+
+            # ファイルの内容をPandasデータフレームに変換
+            if re_prefixed_filename.endswith(('.xls', '.xlsx')):
+                df = pd.read_excel(filename, engine='openpyxl', index_col=None, header=None)
+            elif re_prefixed_filename.endswith('.csv'):
+                df = pd.read_csv(filename, header=None, encoding='shift_jis')
+
+            # ファイルの内容をunicodedataで正規化
+            df_normalized = df.applymap(lambda x: normalize_data(x) if pd.notna(x) else x)
+
+            # ファイルを正規化されたファイル名で保存
+            re_prefixed_filepath = os.path.join(app.config['UPLOAD_FOLDER'], re_prefixed_filename)
+            if re_prefixed_filename.endswith(('.xls', '.xlsx')):
+                df_normalized.to_excel(re_prefixed_filepath, index=False, header=False)
+            elif re_prefixed_filename.endswith('.csv'):
+                df_normalized.to_csv(re_prefixed_filepath, index=False, header=False, encoding='utf-8-sig')
+
+            os.remove(filename)
+            # 正規化されたファイルを直接レスポンスの本文に含めて返す
+            return send_file(re_prefixed_filepath, as_attachment=True)
+    except Exception as e:
+        return str(e), 400
 
 if __name__ == "__main__":
     app.run()
